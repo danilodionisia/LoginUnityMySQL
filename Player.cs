15de27e183998925b0db6aa11120788e63f0ed66
Danilo Dionisia <danilo.dionisia@yahoo.com.br>
2018-10-04 10:53:24 -0300

    Add files via upload

---
+++ Player.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour {
+
+    public Text textLogin, textPoints, textLives;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        textLogin.text = "Name: " + BD.stringLogin;
+        textPoints.text = "Points: " + BD.intPoints.ToString();
+        textLives.text = "Lives: " + BD.intLives.ToString();
+	}
+}
